6a602d208c384414ffd7c38549dedea7ce533575
Aunurrofiq08 <aunurrofiqfz@gmail.com>
2024-03-11 19:36:06 +0700

    Pratikum-1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,13 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index($id, $name) {
-        return view('user')
-            ->with('id', $id)
-            ->with('name', $name);
-    }
+    // public function index($id, $name) {
+        // return view('user')
+        //     ->with('id', $id)
+        //     ->with('name', $name);
+        public function index()
+        {
+
+            $data = [
+                'level_id' => 2,
+                'username' => 'manager tiga',
+                'nama' => 'manager 3',
+                'password' => Hash::make('12345')
+
+            ];
+            UserModel::create($data);
+
+
+            $user = UserModel::all();
+            return view('user', ['data' => $user]);
+
+
+        }
 }
---
+++ app/Models/USerModel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserModel extends Model
+{
+    use HasFactory;
+
+    /**
+     * Define table name that used in this model
+     */
+    protected $table = 'm_user';
+
+    /**
+     * Define Primary key from m_user table
+     */
+    protected $primaryKey = 'user_id';
+    // protected $fillable = ['level_id','username','nama','password'];
+    protected $fillable = ['level_id','username','nama'];
+}
